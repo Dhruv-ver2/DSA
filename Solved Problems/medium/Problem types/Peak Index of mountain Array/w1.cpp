301e68acdca019a2e2ae37ef06f08d60859bc64c
Dhruv Vaishnav <dhruv2vaishnav@gmail.com>
2025-07-27 19:34:43 +0530

    New level

---
+++ Solved Problems/medium/Problem types/Peak Index of mountain Array/w1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> vec={0,3,8,9,5,2};
+    int n=vec.size();
+    int ans;
+
+    for(int i=1;i<n-1;i++){
+        if(vec[i]>vec[i-1]&& vec[i]>vec[i+1]) ans=i;
+    }
+
+    cout<<"The peak element is "<<vec[ans];
+    return 0;
+}
