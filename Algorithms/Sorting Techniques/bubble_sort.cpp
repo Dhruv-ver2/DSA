1a3d7e335721b37452a9fc98a8dd7e6d093e4f1b
Dhruv Vaishnav <dhruv2vaishnav@gmail.com>
2025-11-04 18:27:27 +0530

    added

--- Algorithms/Sorting Techniques/bubble_sort.cpp
+++ Algorithms/Sorting Techniques/bubble_sort.cpp
@@ -2,33 +2,22 @@
 #include<algorithm>
 using namespace std;
 
+
+
 int main(){
-    int arr[]={4,1,5,2,3};
-    int n = sizeof(arr)/sizeof(arr[0]);
 
-    cout<<"Before sorting: ";
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
-    }
+    int arr[]={4,1,3,2};
+    int n=sizeof(arr)/sizeof(int);
 
-    for(int i=0; i<n-1; i++){
-        bool swapcheck = false;
-        for(int j=0; j<n-i-1; j++){
-            if(arr[j] > arr[j+1]){
-                swapcheck=true;
-                swap(arr[j], arr[j+1]);
-            }
+    for(int i=0;i<n-1;i++){
+
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
         }
-
-        if(!swapcheck) break; // If no swaps were made, the array is sorted
     }
 
-    cout<<"\nAfter sorting: ";
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
+    for(int ele:arr){
+        cout<<ele<<endl;
     }
-
-    return 0;
-
+    
 }
-
